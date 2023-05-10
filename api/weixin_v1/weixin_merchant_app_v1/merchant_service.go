48f248082cc0110b402d3077e09af42af326cbfb
Arise <2778368047@qq.com>
2023-05-10 11:12:09 +0800

    feat: 添加微信用户授权

--- api/weixin_v1/weixin_merchant_app_v1/merchant_service.go
+++ api/weixin_v1/weixin_merchant_app_v1/merchant_service.go
@@ -4,11 +4,22 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
-// GetUserInfoReq 获取微信会员信息，相当于静默登录
-type GetUserInfoReq struct {
-	g.Meta `path:"/:appId/userInfo" method:"get" summary:"用户授权" tags:"WeiXin商户服务"`
-}
-
 type AppAuthReq struct {
 	g.Meta `path:"/:appId/gateway.auth" method:"get" summary:"商户应用授权" tags:"WeiXin商户服务"`
+}
+
+type AuthResReq struct { // AppAuth 应用授权响应接收地址 （后续会是前端地址）
+	g.Meta `path:"/:appId/gateway.authRes" method:"get" summary:"授权响应" tags:"WeiXin"`
+
+	AuthCode  string `json:"auth_code"`
+	ExpiresIn string `json:"expires_in"`
+}
+
+type UserAuthReq struct {
+	g.Meta `path:"/:appId/userAuth" method:"get" summary:"用户授权" tags:"WeiXin商户服务"`
+}
+
+// GetUserInfoReq 获取微信会员信息，相当于静默登录
+type GetUserInfoReq struct {
+	g.Meta `path:"/:appId/userInfo" method:"get" summary:"获取用户信息" tags:"WeiXin商户服务"`
 }
--- weixin_controller/merchant/userInfo.go
+++ weixin_controller/merchant/userInfo.go
@@ -61,3 +61,31 @@ func (c *cUserInfo) GetUserInfo(ctx context.Context, _ *weixin_merchant_app_v1.G
 }
 
 // 我们的系统是第三方代调用，那么操作用户授权登陆，AppId是第三方应用的还是商家应用的
+
+// UserAuth 用户授权
+//func (c *cUserInfo) UserAuth(ctx context.Context, _ *weixin_merchant_app_v1.GetUserInfoReq) (api_v1.StringRes, error) {
+//	pathAppId := g.RequestFromCtx(ctx).Get("appId").String()
+//	appIdLen := len(pathAppId)
+//	subAppId := gstr.SubStr(pathAppId, 2, appIdLen) // caf4b7b8d6620f00
+//
+//	appId := "wx" + utility.Base32ToHex(subAppId)
+//
+//	app, err := weixin_service.MerchantAppConfig().GetMerchantAppConfigByAppId(ctx, appId)
+//
+//	//app, err := weixin_service.ThirdAppConfig().GetThirdAppConfigByAppId(ctx, appId)
+//
+//	if err != nil {
+//		return "", err
+//	}
+//
+//	authURL, err := buildAuthURL(app.AppCallbackUrl, appId)
+//
+//	// https:www.kuaimk.com/weixin/$APPID$/wx56j8q12l89h99/gateway.callback
+//	// https://www.kuaimk.com/weixin/$APPID$/wx56j8q12l89h99/gateway.callback
+//
+//	fmt.Println(authURL)
+//
+//	g.RequestFromCtx(ctx).Response.RedirectTo(authURL)
+//
+//	return "success", nil
+//}
